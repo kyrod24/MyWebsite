e75783ad5c65d533a9b28bdd5d8fb0d539957d89
KyleHall <kylewhall@yahoo.com>
2024-11-11 14:21:49 -0600

    Add files via upload
    These are the central files that make up the website as it currently stands. This will also allow me to practice with GitHub committing and merging. This will be an ever-running project for me.

---
+++ main.js
@@ -0,0 +1,7 @@
+import './Styles/styles.css';
+import './Styles/utils.css';
+import './Styles/components/header.css';
+import './Styles/components/introduction.css';
+import './Styles/components/about.css';
+
+
